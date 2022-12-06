57f625fa52d41a41e21d90887391c23ccb4bf4ac
artuguen28 <87493045+artuguen28@users.noreply.github.com>
2022-12-06 13:47:42 -0400

    c refactoring

--- WeaselProgram.c
+++ WeaselProgram.c
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define GENERATIONS 100                    // Number of offspring per generation
-#define MUTATEPROB 0.05                  // Probability of mutation per letter per copy
+#define GENERATIONS 100
+#define MUTATEPROB 0.05
 
-float rnd(void)
-{ // Return random number between 0 and 1.
+float randomFloatGenerator()
+{
     return (float)rand() / RAND_MAX;
 }
+
 int randomIntGenerator(int max)
 {
-    int result = rnd() * (max + 1);
+    int result = randomFloatGenerator() * (max + 1);
     result %= max + 1;
     return result;
 }
@@ -20,89 +21,113 @@ int randomIntGenerator(int max)
 char randomCharGenerator()
 {
     int c = randomIntGenerator(26);
-    if (c == 0) return ' ';
-    else return (char)(c + 64);
+    if (c == 0)
+        return ' ';
+    else
+        return (char)(c + 64);
 }
 
-void weaselProgram(char *phrase)
+int firstScore(char *current, char *phrase)
 {
-    char *current;
-    char *generations[GENERATIONS];
-    int score[GENERATIONS];
-    int generation;
-    int n;
-    int i;
-    int highestscorer;
-    int startscore;
-    srand((int)time(NULL)); // Random seed...
-    current = malloc(strlen(phrase) + 1); // +1 to hold the terminating null character.
-    assert(current != NULL);
+    int startscore = 0;
 
-    for (n = 0; n < GENERATIONS; n++)
-    { // Initialise the offspring strings.
-        generations[n] = malloc(strlen(phrase) + 1);
-        assert(generations[n] != NULL);
-    }
-
-    for (i = 0; i < strlen(phrase); i++)
-    { // Randomise the starting string.
-        current[i] = randomCharGenerator();
-    }
-    current[i] = '\0';
-    startscore = 0; // Work out the score of generation zero.
-    for (i = 0; i < strlen(phrase); i++)
+    // Work out the score of generation zero.
+    for (int i = 0; i < strlen(phrase); i++)
     {
         if (current[i] == phrase[i])
         {
             startscore++;
         }
     }
-    generation = 0;
-    printf("%3d: %s", generations, current);
-    printf(" -- score: %d\n", startscore);
+
+    return startscore;
+}
+
+void weaselProgram(char *phrase)
+{
+    char *current;
+    char *generations[GENERATIONS];
+
+    int score[GENERATIONS];
+    int currentgen;
+    int n, i;
+    int highestscorer, startscore;
+
+    srand((int)time(NULL));
+    current = malloc(strlen(phrase) + 1);
+    assert(current != NULL);
+
+    // Initialise the generations strings.
+    for (n = 0; n < GENERATIONS; n++)
+    {
+        generations[n] = malloc(strlen(phrase) + 1);
+        assert(generations[n] != NULL);
+    }
+
+    // Randomise the starting string.
+    for (i = 0; i < strlen(phrase); i++)
+    {
+        current[i] = randomCharGenerator();
+    }
+    current[i] = '\0';
+    startscore = firstScore(current, phrase);
+
+    currentgen = 0;
+    printf("Generation %3d: %s", currentgen, current);
+    printf(" | Word Score: %d\n", startscore);
 
     while (1)
     {
-        generation++;
+        currentgen++;
+        // Do for each new string...
         for (n = 0; n < GENERATIONS; n++)
-        { // Do for each new string...
+        {
             score[n] = 0;
+            // Do for each letter...
             for (i = 0; i < strlen(phrase); i++)
-            { // Do for each letter...
-                if (rnd() < MUTATEPROB)
-                { // Mutate or just copy.
-                    generations[n][i] = randomchar();
+            {
+                // Mutate or just copy.
+                if (randomFloatGenerator() < MUTATEPROB)
+                {
+                    generations[n][i] = randomCharGenerator();
                 }
                 else
                 {
                     generations[n][i] = current[i];
                 }
-                if (phrase[i] == generation[n][i])
-                { // Keep score.
+
+                // Keep score.
+                if (phrase[i] == generations[n][i])
+                {
                     score[n]++;
                 }
             }
             generations[n][i] = '\0';
         }
+
         highestscorer = 0;
+        // Find highest scoring new string.
         for (n = 1; n < GENERATIONS; n++)
-        { // Find highest scoring new string.
+        {
             if (score[n] > score[highestscorer])
             {
                 highestscorer = n;
             }
         }
-        strcpy(current, generations[highestscorer]);     // Make best string into "current" string
-        printf("%3d: %s", generation, current);          // Print it.
-        printf(" -- score: %d\n", score[highestscorer]); // Print its score.
-        if (strcmp(current, phrase) == 0) break;
-
+        strcpy(current, generations[highestscorer]);
+        printf("Generation %3d: %s", currentgen, current);
+        printf(" | Word Score: %d\n", score[highestscorer]);
+        if (strcmp(current, phrase) == 0)
+            break;
     }
-
-    return 0;
 }
 
 int main(void)
 {
-    
+    // METHINKS IT IS LIKE A WEASEL
+    char *phrase;
+    printf("Type a 28 characters string (only uppercase and spaces):\n");
+    weaselProgram(phrase);
+
+    return 1;
 }
